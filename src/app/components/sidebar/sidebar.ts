a94884879bd6d0cca98700c690bc6f4340709dbe
Oleksandr Chekh <cheh170804@gmail.com>
2025-08-31 23:47:15 +0300

    sidebar improvement

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -8,11 +8,14 @@ import {MenuItem} from './models/menu-item.interface';
 import {NzIconDirective} from 'ng-zorro-antd/icon';
 import {User} from '../../models/user.interface';
 import {USER_INFO} from '../../utils/constants/USER_INFO';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink, NavigationEnd} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
   imports: [
+    CommonModule,
     NzMenuDirective,
     NzMenuItemComponent,
     NzSiderComponent,
@@ -28,8 +31,26 @@ export class Sidebar {
   public readonly menuItems: MenuItem[] = MENU_ITEMS;
   public readonly userInfo: User = USER_INFO;
   public isSidebarCollapsed = signal(false);
+  public currentRoute = signal('');
+
+  constructor(private router: Router) {
+    this.currentRoute.set(this.router.url);
+    this.subscribeOnRouteChanges();
+  }
 
   onCollapseChange(isCollapsed: boolean) {
     this.isSidebarCollapsed.set(isCollapsed);
+  }
+
+  isActiveRoute(menuItem: MenuItem): boolean {
+    return this.currentRoute() === menuItem.link;
+  }
+
+  private subscribeOnRouteChanges(): void {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.currentRoute.set(event.url);
+      });
   }
 }
--- src/app/utils/constants/MENU_ITEMS.ts
+++ src/app/utils/constants/MENU_ITEMS.ts
@@ -4,46 +4,38 @@ export const MENU_ITEMS: MenuItem[] = [
   {
     label: 'Lorem',
     iconPath: 'dashboard',
-    link: '/'
   },
   {
     label: 'Lorem',
     iconPath: 'warning-mark',
-    link: '/'
   },
   {
     label: 'Lorem',
     iconPath: '3d-block',
-    link: '/'
   },
   {
-    label: 'DashboardService',
+    label: 'Dashboard',
     iconPath: 'expand-icon',
     link: '/dashboard'
   },
   {
     label: 'Lorem',
     iconPath: 'tools',
-    link: '/'
   },
   {
     label: 'Lorem',
     iconPath: 'document',
-    link: '/'
   },
   {
     label: 'Lorem',
     iconPath: 'histogram',
-    link: '/'
   },
   {
     label: 'Lorem',
     iconPath: 'settings',
-    link: '/'
   },
   {
     label: 'Lorem',
     iconPath: 'user-badge',
-    link: '/'
   },
 ];
